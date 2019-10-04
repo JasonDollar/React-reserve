81a70a180632031e85cf6a78c3989f32b69712e7
JasonDollar <elrey33@gmail.com>
2019-10-04 18:30:55 +0200

    connection to database

---
+++ utils/connectDb.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const connection = {}
+
+const connectDb = async () => {
+  if (connection.isConnected) {
+    // use existiing db connection
+    console.log('using existing connection')
+    return
+  }
+  // use new database connection
+  const db = await mongoose.connect(process.env.MONGO_SRV, {
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  console.log('db connected')
+  connection.isConnected = db.connections[0].readyState
+}
+
+export default connectDb
