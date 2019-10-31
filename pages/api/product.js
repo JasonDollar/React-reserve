09393072718ba1f0dcfc23ab59c83785f31a1b89
JasonDollar <elrey33@gmail.com>
2019-10-31 13:06:07 +0100

    handle delete product

--- pages/api/product.js
+++ pages/api/product.js
@@ -4,11 +4,46 @@ import connectDb from '../../utils/connectDb'
 
 connectDb()
 
-export default async (req, res) => {
-  const { _id } = req.query
-  const product = await Product.findOne({ _id })
-  if (!product) {
-    return res.status(404).json({ message: 'Product not found', product: null })
+const handleGetRequest = async (req, res) => {
+  try {
+    const { _id } = req.query
+    const product = await Product.findOne({ _id })
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found', product: null })
+    }
+    res.status(200).json({ product })
+  } catch (e) {
+    res.status(500).json({ message: 'error', product: null })
   }
-  res.status(200).json({ product })
 }
+
+const handleDeleteRequest = async (req, res) => {
+  try {
+    const { _id } = req.query
+    const product = await Product.findOneAndDelete({ _id })
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found', product: null })
+    }
+    res.status(204).json({})
+
+  } catch (e) {
+    res.status(500).json({ message: 'error' })
+  }
+}
+
+export default async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      await handleGetRequest(req, res)
+      break
+    case 'DELETE':
+      await handleDeleteRequest(req, res)
+      break
+    default:
+      // 405 - kiedy nie jest to odpowiednia metoda
+      res.status(405).send(`Method ${req.method} not allowed`)
+      break
+  }
+
+}
+
